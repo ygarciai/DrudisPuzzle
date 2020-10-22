188e0414f2f54be1ee4ba4e6a724c8d4923e013e
ygarciai <62152443+ygarciai@users.noreply.github.com>
2020-10-22 17:07:11 +0200

    1
    1

--- app/src/main/java/com/example/drudispuzzle/MainActivity.java
+++ app/src/main/java/com/example/drudispuzzle/MainActivity.java
@@ -16,8 +16,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button btn = (Button) findViewById(R.id.botoninicio);
         Button btn2= findViewById(R.id.salir);
+        Button btn3 = (Button) findViewById(R.id.button);
         btn.setOnClickListener(this);
         btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
         nombre= (EditText) findViewById (R.id.nombreplayer );
 
         if (getIntent().getBooleanExtra("EXIT", false))
@@ -34,9 +36,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 finish();
                 break;
             case R.id.botoninicio:
-                Intent intent = new Intent(view.getContext(), PuzzleActivity.class);
+                Intent intent = new Intent(view.getContext(), ElegirPuzzle.class);
                 intent.putExtra("nombrejugador",nombre.getText().toString());
                 startActivityForResult(intent, 0);
+                break;
+            case R.id.button:
+                Intent intent1 = new Intent(view.getContext(), SelectionActivity.class);
+                startActivityForResult(intent1, 0);
                 break;
         }
 
--- app/src/main/java/com/example/drudispuzzle/SelectionActivity.java
+++ app/src/main/java/com/example/drudispuzzle/SelectionActivity.java
@@ -1,19 +1,22 @@
 package com.example.drudispuzzle;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SelectionActivity#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SelectionActivity extends Fragment {
+public class SelectionActivity  extends AppCompatActivity implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -42,23 +45,50 @@ public class SelectionActivity extends Fragment {
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        setContentView(R.layout.fragment_selection_activity);
+        Button btnPlay = (Button) findViewById(R.id.button_inicioPartida);
+        Button btnRank = (Button) findViewById(R.id.button_ranking);
+        Button btnMult = (Button) findViewById(R.id.button_Multiplayer);
+        Button btnFoto = (Button) findViewById(R.id.button_tomarFoto);
+        btnPlay.setOnClickListener(this);
+        btnRank.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnFoto.setOnClickListener(this);
     }
 
-    @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_selection_activity, container, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_inicioPartida:
+                Intent intent = new Intent(view.getContext(), ElegirPuzzle.class);
+                startActivityForResult(intent, 0);
+                break;
+            case R.id.button_ranking:
+                Intent intent1 = new Intent(view.getContext(), Ranking.class);
+                startActivityForResult(intent1, 0);
+                break;
+            case R.id.button_Multiplayer:
+                Intent intent2 = new Intent(view.getContext(), Multiplayer.class);
+                startActivityForResult(intent2, 0);
+                break;
+            case R.id.button_tomarFoto:
+                Intent intent3 = new Intent(view.getContext(), TomarFoto.class);
+                startActivityForResult(intent3, 0);
+                break;
+        }
+
     }
 }
