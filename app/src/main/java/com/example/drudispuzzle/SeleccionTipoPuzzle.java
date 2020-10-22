9620eedabccc822952169dbbb87b9065c8190a73
Isa <mgilmol@uoc.edu>
2020-10-22 21:15:34 +0200

    4
    4

--- app/src/main/java/com/example/drudispuzzle/SeleccionTipoPuzzle.java
+++ app/src/main/java/com/example/drudispuzzle/SeleccionTipoPuzzle.java
@@ -37,7 +37,7 @@ public class SeleccionTipoPuzzle  extends AppCompatActivity implements View.OnCl
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1XDDDDDDDDDDDDDDDDDDDDDDDDDD.
+     * @param param1 Parameter 1.
      * @param param2 Parameter 2.
      * @return A new instance of fragment SeleccionTipoPuzzle.
      */
