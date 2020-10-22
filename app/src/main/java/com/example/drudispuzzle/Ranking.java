399fcc98507c767a5189e5a1551cc178e1ad7dfb
Isa <mgilmol@uoc.edu>
2020-10-22 21:09:29 +0200

    2

--- app/src/main/java/com/example/drudispuzzle/Ranking.java
+++ app/src/main/java/com/example/drudispuzzle/Ranking.java
@@ -33,7 +33,7 @@ public class Ranking extends AppCompatActivity {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
+     * @param param1 Parameter 1b.
      * @param param2 Parameter 2a.
      * @return A new instance of fragment Ranking.
      */
