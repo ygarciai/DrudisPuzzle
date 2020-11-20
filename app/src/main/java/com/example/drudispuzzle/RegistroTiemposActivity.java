f908c35ebc4fbc33543ccbc279e5f8fc8d30e82f
Jordi-uoc <64849715+Jordi-uoc@users.noreply.github.com>
2020-11-20 17:17:36 +0100

    .

--- app/src/main/java/com/example/drudispuzzle/RegistroTiemposActivity.java
+++ app/src/main/java/com/example/drudispuzzle/RegistroTiemposActivity.java
@@ -1,8 +1,10 @@
 package com.example.drudispuzzle;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,6 +17,7 @@ public class RegistroTiemposActivity extends AppCompatActivity {
     EditText campoName, campoTime;
 
     ConexionSQLiteHelper conn;
+    private View CalenRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +29,15 @@ public class RegistroTiemposActivity extends AppCompatActivity {
         // no es EditText pues sera el tiempo del contador
         campoTime= (EditText) findViewById(R.id.campoTime);
 
+        //añado CalenRegistro y la funcion RegCalendar()
+        CalenRegistro = findViewById(R.id.btnRegistro);
+
     }
 
     public void onClick (View view) {
         registroRecords();
         registroRecordsSql();
+        RegCalendar();
 
     }
 
@@ -64,5 +71,27 @@ public class RegistroTiemposActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),"Tiempo resultante: "+idResultante, Toast.LENGTH_SHORT);
         db.close();
 
+    }
+
+    private void RegCalendar() {
+        CalenRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!campoName.getText().toString().isEmpty() && !campoTime.getText().toString().isEmpty()) {
+
+                    Intent intent = new Intent(Intent.ACTION_INSERT);
+                    intent.setData(CalendarContract.Events.CONTENT_URI);
+                    intent.putExtra(CalendarContract.Events.ACCOUNT_NAME, campoName.getText().toString());
+                    intent.putExtra(CalendarContract.Events.EVENT_TIMEZONE, campoTime.getText().toString());
+                    intent.putExtra(CalendarContract.Events.ALL_DAY, "true");
+
+                    if(intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
+
+                }
+
+            }
+        });
     }
 }
