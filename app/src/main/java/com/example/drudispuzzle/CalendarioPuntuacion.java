c6940bba672114fadfc21817d6edac6bcd730c5c
Jordi-uoc <64849715+Jordi-uoc@users.noreply.github.com>
2020-11-20 12:19:56 +0100

    .

---
+++ app/src/main/java/com/example/drudispuzzle/CalendarioPuntuacion.java
@@ -0,0 +1,4 @@
+package com.example.drudispuzzle;
+
+public class CalendarioPuntuacion {
+}
--- app/src/main/java/com/example/drudispuzzle/PartidaSinFragmentar.java
+++ app/src/main/java/com/example/drudispuzzle/PartidaSinFragmentar.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.provider.CalendarContract;
 import android.text.method.Touch;
 import android.util.Log;
 import android.view.DragEvent;
@@ -252,6 +253,11 @@ public class PartidaSinFragmentar extends AppCompatActivity implements View.OnCl
             chronometro.setText(tiempo);
 
             btnGuardar.setOnClickListener(this);
+
+            //añado para el calendario
+            Intent intent = new Intent(Intent.ACTION_INSERT);
+            intent.setData(CalendarContract.Events.CONTENT_URI);
+            intent.putExtra(CalendarContract.Events.TITLE, chronometro.getText().toString());
 
         }
 
